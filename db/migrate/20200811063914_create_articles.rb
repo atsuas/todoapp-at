1c0db5248b276b4dd3dabb826e39b74287ae901a
as <>
2020-08-12 07:26:43 +0900

    article migrate

--- db/migrate/20200811063914_create_articles.rb
+++ db/migrate/20200811063914_create_articles.rb
@@ -1,7 +1,8 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-      
+      t.string :title
+      t.text :cotent
       t.timestamps
     end
   end
