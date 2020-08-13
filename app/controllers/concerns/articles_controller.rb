6eb304a0b169abb4e327365223cc51bf5e803551
as <>
2020-08-13 14:53:24 +0900

    update機能を追加

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -2,9 +2,11 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
     def show
         @article = Article.find(params[:id])
     end
+
     def new
         @article = Article.new
     end
@@ -22,9 +24,17 @@ class ArticlesController < ApplicationController
     def edit
         @article = Article.find(params[:id])
     end
+
     def update
-        
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+        redirect_to article_path(@article), notice: 'update successful!!'
+        else
+            flash.now[:error] = 'update failed'
+            render :edit
+        end
     end
+
     private
     def article_params
         params.require(:article).permit(:title, :content)
