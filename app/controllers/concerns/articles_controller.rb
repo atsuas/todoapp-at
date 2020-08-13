37de6861697b7e0c7ec3b02e3477a15d6ae4ae7b
as <>
2020-08-13 14:42:54 +0900

    editページのcss調整

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -18,7 +18,13 @@ class ArticlesController < ApplicationController
           render :new
         end
     end
-    
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def update
+        
+    end
     private
     def article_params
         params.require(:article).permit(:title, :content)
