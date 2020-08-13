4b153d53a37b59a9a4ad3fed0bd29445466be3e3
as <>
2020-08-13 09:29:17 +0900

    編集ボタン追加

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -8,5 +8,19 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
-
+    
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+          redirect_to article_path(@article), notice: 'Save complete'
+        else
+          flash.now[:error] = 'save error'
+          render :new
+        end
+    end
+    
+    private
+    def article_params
+        params.require(:article).permit(:title, :content)
+    end
 end
