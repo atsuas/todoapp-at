84fc4176a73f6805d767b0bad7fa015896ccc4d3
as <>
2020-08-16 10:12:07 +0900

    ユーザーと記事を紐付け

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
 
+    belongs_to :model
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_063914) do
+ActiveRecord::Schema.define(version: 2020_08_16_001835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_08_11_063914) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "model_id"
+    t.index ["model_id"], name: "index_articles_on_model_id"
   end
 
   create_table "models", force: :cascade do |t|
