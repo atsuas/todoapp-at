0a01b30636d5a9077b4b2db78fe055d43cf7c188
as <>
2020-08-20 16:12:52 +0900

    cardを表示、herokuの設定

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,6 +1,22 @@
 class CardsController < ApplicationController
 
     def new
-        
+        article = Article.find(params[:article_id])
+        @card = article.cards.build
+    end
+
+    def create
+        article = Article.find(params[:article_id])
+        @card = article.cards.build(card_params)
+        if @card.save
+          redirect_to article_path(article), notice: 'Add card'
+        else
+          flash.now[:error] = 'Could not update'
+          render :new
+        end
+      end
+    private
+    def card_params
+        params.require(:card).permit(:title, :content)
     end
 end
---
+++ app/models/card.rb
@@ -0,0 +1,4 @@
+class Card < ApplicationRecord
+    belongs_to :article
+    validates :content, presence: true
+end
